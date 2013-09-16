9d341030e0eaae9628ea5c22e7b2f09a139c4c54
liuyijiang <liuyijiang3430@qq.com>
2013-09-16 16:27:14 +0800

    dwadad

---
+++ src/com/spring/bootstarp/BootstarpController.java
@@ -0,0 +1,15 @@
+package com.spring.bootstarp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class BootstarpController {
+
+	@RequestMapping(value = "/bindex", method = RequestMethod.GET)
+	public ModelAndView bindex(){
+		return new ModelAndView("bindex.jsp");
+	}
+}
