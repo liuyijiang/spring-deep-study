fc38b4ac1c395e326c6417bd67a210e263bade96
liuyijiang <liuyijiang3430@qq.com>
2013-12-18 09:31:47 +0800

    dwada

--- src/com/http/TestGetUrl.java
+++ src/com/http/TestGetUrl.java
@@ -4,8 +4,10 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -33,8 +35,8 @@ public class TestGetUrl {
 	public static void main(String[] args) throws ClientProtocolException, IOException {
 		TestGetUrl t = new TestGetUrl();
 		//t.testUrl();
-		t.testGetUrlByParm("http://music.baidu.com/song/10525914/download", ".mp3");
-		//t.testGetUrlByParm("http://music.baidu.com/song/1008046/download", ".mp3");
+		//t.testGetUrlByParm("http://music.baidu.com/song/10525914/download", ".mp3");
+		t.testGetUrlByParm("http://music.baidu.com/song/1008046/download", ".mp3");
 		//t.testGetUrlByParm("http://music.baidu.com/song/7300733/download", ".mp3");
         //t.testHttp(); //http://music.baidu.com/song/7300733/download
 	}
@@ -47,17 +49,17 @@ public class TestGetUrl {
 		Map<String, String> head = new HashMap<String, String>();
 		Set<String> set = new HashSet<String>();
 		Connection conn = Jsoup.connect(url);
-		conn.cookie("Cookie", "	BAIDUID=447A19077A8C8BCFAC7A138A210EF4A0:FG=1; MCITY=-240%3A; BDUSS=RwMEM1and1SlA2YUg0aWp3dlZHSFBlbmRBa3piZ0hZZm9ZTnJEMUxTU0laa0pTQVFBQUFBJCQAAAAAAAAAAAEAAADKKvcWxua8o-bD5sMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIjZGlKI2RpSZE;");
+		conn.cookie("Cookie", "BAIDUID=447A19077A8C8BCFAC7A138A210EF4A0:FG=1; MCITY=-240%3A; BDUSS=RwMEM1and1SlA2YUg0aWp3dlZHSFBlbmRBa3piZ0hZZm9ZTnJEMUxTU0laa0pTQVFBQUFBJCQAAAAAAAAAAAEAAADKKvcWxua8o-bD5sMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIjZGlKI2RpSZE;");
 		conn.timeout(5000);
 		try {
 			Document doc = conn.get();
 			Elements a = doc.select("a[href~=.mp3]"); // 具有 href 属性的链接
 			//Elements a = doc.select("a[href]"); // 具有 href 属性的链接
-			System.out.println(1);
-			for (Element e : a) {
-				System.out.println(e.attr("href"));
-				//set.add(e)
-			}
+			System.out.println(a.last().attr("id"));
+//			for (Element e : a) {
+//				System.out.println(e.attr("href"));
+//				//set.add(e)
+//			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -65,11 +67,23 @@ public class TestGetUrl {
 
 	public void testUrl(){
 		String url1 = "http://www.11816912.cn/music/101505.mp3";
-		String u = "/data/music/file?link=http://zhangmenshiting.baidu.com/data2/music/10525915/105259141387184461128.mp3?xcode=69bee0bed75698cba0e58adaeb5dcbd60fb35909493b340e&song_id=10525914";
-	    String ss = u.substring(u.indexOf("http"),u.length());
-	    String ss2 = url1.substring(url1.indexOf("http"),url1.length());
-	    System.out.println(ss);
-	    System.out.println(ss2);
+		String url2 = "http://www.11816912.cn/music/101505.mp3";
+		String u1 = "/data/music/file?link=http://zhangmenshiting.baidu.com/data2/music/10525915/105259141387184461128.mp3?xcode=69bee0bed75698cba0e58adaeb5dcbd60fb35909493b340e&song_id=10525914";
+		String u2 = "/data/music/file?link=http://zhangmenshiting.baidu.com/data2/music/10525915/105259141387184461254.mp3?xcode=69bee0bed75698cba0e58adaeb5dcbd60fb35909493b340e&song_id=10525914";
+		String u3 = "/data/music/file?link=http://zhangmenshiting.baidu.com/data2/music/10525915/105259141387184461320.mp3?xcode=69bee0bed75698cba0e58adaeb5dcbd60fb35909493b340e&song_id=10525914";
+//		String ss = u.substring(u.indexOf("http"),u.length());
+//	    String ss2 = url1.substring(url1.indexOf("http"),url1.length());
+//	    System.out.println(ss);
+//	    System.out.println(ss2);
+//	    Set<String> set = new HashSet<String>();
+		List<String> set = new ArrayList<String>();
+	    set.add(u1);
+	    set.add(u2);
+	    set.add(u3);
+	    System.out.println(set.get(set.size() - 1));
+//	    for (String st:set) {
+//	    	System.out.println(st);
+//	    }
 	}
 	
 	
